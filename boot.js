a432b7d48797952ca81e8bacc18348292d9e3c42
NgocTV <tongoc92@gmail.com>
2017-02-16 11:33:43 +0700

    install passportjs

--- boot.js
+++ boot.js
@@ -4,6 +4,9 @@ const config          = require('./config');
 const nunjuckProvider = require('./shared/nunjuck.provider');
 const mongodbProvider = require('./shared/mongodb.provider');
 const errorProvider   = require('./shared/error.provider');
+const passport        = require('koa-passport');
+const koaSession      = require('koa-session');
+const bodyParser      = require('koa-bodyparser');
 
 // service
 const ArticleServiceProvider = require('./fanstatic/article/article.service-provider');
@@ -11,11 +14,16 @@ const ProfileServiceProvider = require('./fanstatic/profile/profile.service-prov
 
 module.exports = (app) => {
     app.context.config = config;
+    app.use(bodyParser());
     app.use(nunjuckProvider);
     app.use(mongodbProvider(config.database.mongodb));
     app.use(errorProvider);
     app.use(ArticleServiceProvider);
     app.use(ProfileServiceProvider);
+    app.use(koaSession(app));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
     
     return app;
 };
