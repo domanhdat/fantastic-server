edc8b151fc34214c9d8231713249aa572ec3d967
NgocTV <tongoc92@gmail.com>
2017-03-03 23:02:18 +0700

    class Identity

--- fanstatic/authentication/credential/identity.js
+++ fanstatic/authentication/credential/identity.js
@@ -6,7 +6,25 @@ class Identity {
         this.attributes = {}
     }
 
+    set type(value) {
+        this.attributes.type = value;
+    }
 
+    get type() {
+        return this.attributes
+    }
+
+    set(attrName, attrValue) {
+        this.attributes[attrName] = attrValue;
+    }
+
+    get(attrName) {
+        return this.attributes[attrName];
+    }
+
+    toJson() {
+        return this.attributes
+    }
 }
 
 module.exports = Identity;
